946c23e06c50a5daf556a7fab8c60fe3785ce4a1
Adam <adam@olivegren.se>
2019-10-30 23:06:33 +0100

    swipeout finished

--- src/components/groceryListsContainer.js
+++ src/components/groceryListsContainer.js
@@ -12,6 +12,7 @@ import PropTypes from 'prop-types';
 import Swipeout from '../components/swipeout';
 import textStyles from '../styles/textStyles';
 import {KeyboardAwareFlatList} from 'react-native-keyboard-aware-scroll-view';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 class GroceryListItem extends React.Component {
   state = {
@@ -25,11 +26,14 @@ class GroceryListItem extends React.Component {
           var {height, width} = event.nativeEvent.layout;
           this.setState({viewWidth: width, viewHeight: height});
         }}
-        style={GroceryListItemStyles.container}
+        style={[GroceryListItemStyles.container]}
         underlayColor="transparent"
         fontSize={50}
         onPress={() => this.props.goToGroceryList(this.props.item)}>
-        <Swipeout viewWidth={this.state.viewWidth}>
+        <Swipeout
+          viewWidth={this.state.viewWidth}
+          onPress={() => this.props.goToGroceryList(this.props.item)}
+          delete={() => this.props.removeGroceryList()}>
           <View style={GroceryListItemStyles.container2}>
             <Text style={textStyles.default}>{this.props.item.title}</Text>
             <View style={GroceryListItemStyles.badge}>
@@ -50,6 +54,7 @@ export default class GroceryListsContainer extends React.Component {
         item={list}
         goToGroceryList={this.props.goToGroceryList}
         numberOfItems={this.props.numberOfItems}
+        removeGroceryList={() => this.props.removeGroceryList({list})}
       />
     );
   }
@@ -72,7 +77,6 @@ const GroceryListItemStyles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     width: '97%',
-    padding: '2%',
     marginLeft: '3%',
     marginBottom: '3%',
     alignSelf: 'center',
@@ -83,8 +87,7 @@ const GroceryListItemStyles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingLeft: '3%',
-    paddingRight: '3%',
+    paddingHorizontal: '3%',
   },
   swipeout: {
     backgroundColor: 'transparent',
