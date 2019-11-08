1a3e60dc87e572a3d12978d8cc55a441b4ce601f
Eric <eric.borjesson@hotmail.com>
2019-11-08 18:13:57 +0100

    apollo client setup

--- client/index.js
+++ client/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {ApolloProvider} from '@apollo/react-hooks';
+
 import App from './App';
 import {name as appName} from './app.json';
 
-const AppWrapper = () => <App />;
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+const AppWrapper = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
 
 AppRegistry.registerComponent(appName, () => AppWrapper);
