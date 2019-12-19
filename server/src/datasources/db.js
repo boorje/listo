b7d23ad7d541c5b8dda2b0e891d3b23738688965
Eric <eric.borjesson@hotmail.com>
2019-12-19 18:46:33 +0100

    added editorcount on list create

--- server/src/datasources/db.js
+++ server/src/datasources/db.js
@@ -62,7 +62,6 @@ class DB extends DataSource {
     });
     let foundList = list.get({ plain: true });
     foundList.owner = foundList.listOwner;
-    console.log(foundList);
     return foundList;
   }
 
@@ -117,6 +116,7 @@ class DB extends DataSource {
     let createdList = res.get({ plain: true });
     createdList.isOwner = true;
     createdList.itemCount = 0;
+    createdList.editorCount = 0;
     const user = await this.store.User.findByPk(this.context.user.id);
     createdList.owner = user.get({ plain: true });
     return createdList;
